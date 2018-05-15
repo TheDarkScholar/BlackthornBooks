48fc80c74e6fefb76f5c5ff6324de16a68d27295
TheDarkScholar <36053889+TheDarkScholar@users.noreply.github.com>
2018-05-15 15:21:06 -0600

    worked on getting two slideshows to work

--- scripts.js
+++ scripts.js
@@ -7,13 +7,23 @@ function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n, no) {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
+  let slides = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName("dot");
   if (n > x.length) {slideIndex[no] = 1}
-    if (n < 1) {slideIndex[no] = x.length}
-      for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
-      }
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   x[slideIndex[no]-1].style.display = "block";
+}
+  dots[slideIndex-1].className += " active";
 }
