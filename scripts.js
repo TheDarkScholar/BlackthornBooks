abb9f8b0635829f2f7071f6a877c9a290660638f
TheDarkScholar <36053889+TheDarkScholar@users.noreply.github.com>
2018-05-15 23:55:54 -0600

    attempted to fix the slideshows

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
@@ -13,17 +11,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName(slideId[no]);
+  let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex[no]-1].style.display = "block";
-}
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
